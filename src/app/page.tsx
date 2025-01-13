bf8e4f3019b0de129d39f5983a26bc32bad8ee43
clickclickcode <101672618+clickclickcode@users.noreply.github.com>
2025-01-13 15:11:38 +0100

    adding a grid to display books

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from "next/image";
+import { mockBooks } from '@/data/mockBooks'
+import { BookCard } from '@/components/BookCard/BookCard'
 
 export default function Home() {
   return (
-    <>Book Tracker</>
+	<main className='container mx-auto p-8'>
+		<h1 className='text-3xl font-bold mb-8'>My Reading List</h1>
+
+		<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+			{mockBooks.map((book) => (
+				<BookCard key={book.id} book={book} />
+			))}
+		</div>
+	</main>
   );
 }
