80bb70b469671c3a7de0dc4da80fbf68b75ec89d
clickclickcode <101672618+clickclickcode@users.noreply.github.com>
2025-01-13 15:19:37 +0100

    copying the mock books to get an idea of how the grid behaves containing more than the initial three books

--- src/data/mockBooks.ts
+++ src/data/mockBooks.ts
@@ -21,5 +21,45 @@ export const mockBooks: Book[] = [
         title: 'Project Hail Mary',
         author: 'Andy Weir',
         status: 'unread'
+    },
+    {
+        id: '4',
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        coverUrl: 'https://images-na.ssl-images-amazon.com/images/I/91b0C2YNSrL.jpg',
+        status: 'finished'
+    },
+    {
+        id: '5',
+        title: 'Dune',
+        author: 'Frank Herbert',
+        coverUrl: 'https://images-na.ssl-images-amazon.com/images/I/91b6bM961mL.jpg',
+        status: 'reading'
+    },
+    {
+        id: '6',
+        title: 'Project Hail Mary',
+        author: 'Andy Weir',
+        status: 'unread'
+    },
+    {
+        id: '7',
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        coverUrl: 'https://images-na.ssl-images-amazon.com/images/I/91b0C2YNSrL.jpg',
+        status: 'finished'
+    },
+    {
+        id: '8',
+        title: 'Dune',
+        author: 'Frank Herbert',
+        coverUrl: 'https://images-na.ssl-images-amazon.com/images/I/91b6bM961mL.jpg',
+        status: 'reading'
+    },
+    {
+        id: '9',
+        title: 'Project Hail Mary',
+        author: 'Andy Weir',
+        status: 'unread'
     }
 ]
