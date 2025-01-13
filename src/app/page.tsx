0f3b2681f79d83483a5478b666bc2cfbf8f36545
clickclickcode <101672618+clickclickcode@users.noreply.github.com>
2025-01-13 16:04:14 +0100

    implementing the FilterBar component and functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ import { useState } from 'react'
 import { mockBooks } from '@/data/mockBooks'
 import { BookCard } from '@/components/BookCard/BookCard'
 import { Book } from '@/types/book.types'
+import { FilterBar } from '@/components/FilterBar/FilterBar'
 
 
 export default function Home() {
 	// setting up state for the books
 	const [books, setBooks] = useState<Book[]>(mockBooks)
+	// setting up state for the filter bar
+	const [filter, setFilter] = useState('all')
 
 	// creating an event handler for the books' status
 	const handleStatusChange = (id: string, newStatus: Book['status']) => {
@@ -19,13 +22,22 @@ export default function Home() {
 		)
 	}
 
+	const filteredBooks = filter == 'all'
+		? books
+		: books.filter(book => book.status === filter)
+
 
 	return (
 		<main className='container mx-auto p-8'>
 			<h1 className='text-3xl font-bold mb-8'>My Reading List</h1>
 
+			<FilterBar
+				currentFilter={filter}
+				onFilterChange={setFilter}
+			/>
+
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-				{books.map((book) => (
+				{filteredBooks.map((book) => (
 					<BookCard
 						key={book.id}
 						book={book}
